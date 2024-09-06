2d781cb18770c7c85e77bad38685e072eeb5eac0
Tiago Coelho <tiagomcoelho22@gmail.com>
2024-09-06 12:41:50 +0100

    Commit 2

--- BasicSaveString/BasicSaveString/SaveStringService.swift
+++ BasicSaveString/BasicSaveString/SaveStringService.swift
@@ -8,5 +8,9 @@
 import Foundation
 
 public protocol SaveStringService {
+    /// Executes a request to the server to store the string
+    ///
+    ///  - Parameters:
+    ///     - myString: The string to be stored
     func execute(myString: String) async throws
 }
